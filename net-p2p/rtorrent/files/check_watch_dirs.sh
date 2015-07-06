4eaea49bae6209ba0091e5c04fdfe8a8677cdef7
deterenkelt <deterenkelt.github@gmail.com>
2015-07-06 09:02:25 +0300

    added nitrotools ebuild

--- net-p2p/rtorrent/files/check_watch_dirs.sh
+++ net-p2p/rtorrent/files/check_watch_dirs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 torrent_files_dir='/home/torrents'
 # ‘Host’ supposed to identify a folder under /home/torrents/
 storage_hosts=( home )
@@ -7,12 +8,12 @@ storages=('/home' ) #'/old_home'
 storage_for_download=0
 # Subfolders under storage_main_dir which directory trees must be kept in sync
 storage_subdirs=(         'brains' 'music'  'video' 'gamefiles' 'misc_torrents' 'picts/manga')
-storage_subdirs_maxdepth=(1        1        2         1           1                0)
+storage_subdirs_maxdepth=(1        1        2       1           1               0)
 watchdir_count=0
 verbose_log='_verbose' # rtorrent verbosity per each rule(?)
 
 # Remove any existing rules for watching directories from the config.
-sed -ri '/^\s*schedule\s*=\s*watch_directory.*/d' /home/$USER/.rtorrent.rc 
+sed -ri '/^\s*schedule\s*=\s*watch_directory.*/d' /home/$USER/.rtorrent.rc
 
 for host in "${storage_hosts[@]}"; do
 	[ "$host" = "${HOSTNAME%%.*}" ] && {
@@ -20,13 +21,13 @@ for host in "${storage_hosts[@]}"; do
 		[ -r "$torrent_files_dir/$host" ] &&
 		[ -w "$torrent_files_dir/$host" ] &&
 		[ -x "$torrent_files_dir/$host" ] ||
-		mkdir --mode=775 "$torrent_files_dir/$host" 
+		mkdir --mode=775 "$torrent_files_dir/$host"
 		chown -R --reference=/home/$USER/.rtorrent.rc "$torrent_files_dir/$host"
 		# stashing all stuff
 		cp -Ra "$torrent_files_dir/$host" "$torrent_files_dir/${host}.bak"
 		# Cleaning the torrent_files_dir from folders deleted in the original storage.
 		while IFS= read -r -d $'\0'; do
-			REPLY=$(echo "$REPLY" | sed "s^$torrent_files_dir/$host/^^g") 
+			REPLY=$(echo "$REPLY" | sed "s^$torrent_files_dir/$host/^^g")
 			[ -e "${storages[0]}/$REPLY" ] || {
 				einfo 'This directory wasn’t found in storage ↓'
 				rm -rfv "$torrent_files_dir/$host/$REPLY"
@@ -35,7 +36,7 @@ for host in "${storage_hosts[@]}"; do
 
 		for ((i=0; i<${#storage_subdirs[@]}; i++)); do
 			while IFS= read -r -d $'\0'; do
-				REPLY=$(echo "$REPLY" | sed "s^${storages[0]}/^^g") 
+				REPLY=$(echo "$REPLY" | sed "s^${storages[0]}/^^g")
 				[ -e "$torrent_files_dir/$host/$REPLY" ] ||
 				mkdir -p --mode=775 "$torrent_files_dir/$host/$REPLY"
 				for ((j=1; j<${#storages[@]}; j++)); do
